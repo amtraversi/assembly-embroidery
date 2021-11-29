fc1534dfd6a2720bdc4374beb39cbb9e66d4ebf2
Angel Traversi <52934337+amtraversi@users.noreply.github.com>
2021-11-29 17:36:57 -0300

    funcion horasSalarioMinimo

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,38 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+function horasSalarioMinimo(){
+let obj = {};
+	obj.salarioMinimo = 30000
+	obj.horasPorDia = 8
+	obj.diasPorSemana = 5
+	obj.semanasPorMes = 31/7
+	obj.horasPorSemana = obj.horasPorDia * obj.diasPorSemana
+	obj.horasPorMes = obj.horasPorSemana * obj.semanasPorMes
+  console.log(obj) 
+ return obj.salarioMinimo / obj.horasPorMes
+}
+/*
+function Pages() {
+  const CanvasColor = {
+    color: 1
+  }
+  DesignColors(CanvasColor)
+  DesignView(CanvasColor)
+
+  function DesignView(CanvasBG) {
+    console.log(CanvasBG)
+  }
+
+  function DesignColors(props) {
+    ColorButton(props)
+
+    function ColorButton(color) {
+      color.color = 2
+    }
+  }
+}*/
+// Pages()
+let a = horasSalarioMinimo()
+console.log(a)
